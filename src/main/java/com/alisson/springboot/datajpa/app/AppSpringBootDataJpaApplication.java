8039b94d6063f00518c5fb89ac64a505b61d660b
Alisson <diane_10_96@hotmail.com>
2020-03-04 17:59:47 -0500

    Create project jpa

---
+++ src/main/java/com/alisson/springboot/datajpa/app/AppSpringBootDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.alisson.springboot.datajpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringBootDataJpaApplication.class, args);
+	}
+
+}
