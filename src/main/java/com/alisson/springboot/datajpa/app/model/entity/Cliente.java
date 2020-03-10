8074ece3a989626401680fad490f417c4250e232
alissondiane <diane_10_96@hotmail.com>
2020-03-10 12:47:13 -0500

    Add Entities

--- src/main/java/com/alisson/springboot/datajpa/app/model/entity/Cliente.java
+++ src/main/java/com/alisson/springboot/datajpa/app/model/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.alisson.springboot.datajpa.app.model.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -42,7 +47,18 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 	
+	//Cascada efecto para eliminar en cascada datos relacionados
+	@OneToMany(mappedBy="cliente",fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	private List<Factura> facturas;
+	
 	private String foto;
+	
+	
+	
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
+	}
+
 	/*
 	@PrePersist
 	public void prePersist() {
@@ -101,6 +117,17 @@ public class Cliente implements Serializable {
 		this.foto = foto;
 	}
 
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
 
 
 	private static final long serialVersionUID = -1878431166304897905L;
