3544e4234b9563f7473204cc855fb4b91904078c
Alisson <diane_10_96@hotmail.com>
2020-12-17 11:07:42 -0500

    update

--- src/main/java/com/alisson/springboot/datajpa/app/controllers/ClienteRestController.java
+++ src/main/java/com/alisson/springboot/datajpa/app/controllers/ClienteRestController.java
@@ -1,10 +1,16 @@
 package com.alisson.springboot.datajpa.app.controllers;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.alisson.springboot.datajpa.app.model.entity.Factura;
 import com.alisson.springboot.datajpa.app.model.service.IClienteService;
 import com.alisson.springboot.datajpa.app.view.xml.ClienteList;
 
@@ -19,5 +25,9 @@ public class ClienteRestController {
 	@GetMapping(value = "/listar")
 	public ClienteList listar() {
 		   return new ClienteList(clienteService.findAll());
+	}
+	@GetMapping("/ver/{id}")
+	public Factura ver(@PathVariable(value="id") Long id) {
+		return clienteService.fetchByIdWithClienteWithItemFacturaWithProcucto(id);
 	}
 }
